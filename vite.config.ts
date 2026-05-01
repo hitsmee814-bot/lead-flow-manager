04319c2f61cb3c573f1e4e1ed735726cabeaf424
Rahul <hitsmee814@gmail.com>
2026-05-01 10:15:51 +0530

    route fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-    base: "/lead-flow-manager/",
+    base: "/lead-flow-manager",
 
 //   server: {
 //     host: "::",
